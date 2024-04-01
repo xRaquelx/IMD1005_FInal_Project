0ef92185e726b8f7749f6e6a3e1c3208f417f70e
Cayden <81267902+dacayed@users.noreply.github.com>
2024-04-01 19:01:53 -0400

    Add files via upload

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,32 +1,52 @@
-import Post from "./Post.js";
+class Post {
+    constructor(title, img, alt, text, link) {
+        this.title = title;
+        this.img = img;
+        this.alt = alt;
+        this.text = text;
+        this.link = link;
+    }
+
+    renderPostCard() {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <div class="card-img">
+            <img src="${this.img}" alt="${this.alt}">
+        </div>
+        <div class="card-info">
+            <h1>${this.title}</h1>
+            <div class="card-info_text">
+                <p>
+                    ${this.text}
+                </p>
+                <div class="fade"></div>
+            </div>
+            <div class="card-link">
+                <a href="${this.link}">Learn more!</a>
+            </div>
+        </div>
+        `;
+        return card;
+    }
+};
 
 const titles = ["Nintendo Switch", "PS5", "A6400 Sony Camera", "Laptop"];
-const imgs = ["/images/nintendo-switch.jpg", "/images/ps5.jpg", "/images/sony-camera.jpg", ""];
+const imgs = ["/images/nintendo-switch.jpg", "/images/ps5.jpg", "/images/sony-camera.jpg"];
 const alts = [
     "Imagine of Nintendo Switch controllers in the front and screen behind",
     "Imagine of PS5 controller and console",
-    "Imagine of A6400 Sony Camera",
-    ""
+    "Imagine of A6400 Sony Camera"
 ];
-
-const texts = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Aliquid doloremque sed impedit est eos! 
-Maxime aperiam inventore impedit culpa aspernatur doloribus dignissimos molestiae 
-repudiandae ipsa perspiciatis unde magni, in quisquam beatae harum possimus rem dolores 
-ratione sequi excepturi fugiat, vel facere quidem minus! Ad deserunt sequi temporibus 
-cumque officiis dolores nemo quia velit laudantium iusto accusamus id porro veritatis 
-expedita iste suscipit quos amet assumenda veniam, inventore et eius similique dolorem!`;
-
+const texts = [];
 const links = [
     "/Switch-article.html",
     "/ps5.html",
     "/sony.html",
-    "/laptop.html"
+    "laptop.html"
 ];
 
-const main = document.querySelector("main");
+const main = document.getElementsByTagName("main");
 
-titles.forEach((title, i) => {  
-    main.appendChild((new Post(title, imgs[i], alts[i], texts, links[i])).renderPostCard());
-});
+
 
